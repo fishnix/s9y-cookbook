1846e58bec38362542d0935729e3f8fc80c21dc9
E Camden Fisher <fish@fishnix.net>
2014-07-01 21:41:39 -0400

    integration tests

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "fish@fishnix.net"
 license          "Apache 2.0"
 description      "Installs/Configures s9y"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 depends "apache2"
 depends "php5"
 depends "git"
